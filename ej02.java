a7d77673385d1dd69dc0c2bf033d634c6d974a0a
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-09-30 16:34:24 +0000

    Subida ej02

---
+++ ej02.java
@@ -0,0 +1,8 @@
+public class ej02 {
+    public static void main(String[] args) {
+        String nombre = "Manuel León Ruiz";
+        int edad = 25;
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+}
