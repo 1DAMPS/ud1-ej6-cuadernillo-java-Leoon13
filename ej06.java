0ae4d37a140660a2f6754814a11f533e7c124b2e
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-09-30 17:16:18 +0000

    Subida ej06

---
+++ ej06.java
@@ -0,0 +1,8 @@
+public class ej06 {
+    public static void main(String[] args) {
+    String miEdad = "Menos de lo que aparento";
+    String miDineroHoy = " No te lo digo ";
+    String miSerieFavorita = "Juego de Tronos";
+    System.out.println(miEdad +  miDineroHoy + miEdad);
+    }
+}
