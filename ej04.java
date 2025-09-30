ae5273a45cd21ee8af4b6bdd2d7ccf9c748f759d
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-09-30 16:57:10 +0000

    Subida ej04

---
+++ ej04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ej04 {
+    public static void main(String[] args) {
+       int numero1 = 3;
+       int numero2 = 4;
+       System.out.println(numero1 + numero2);
+    }
+}
