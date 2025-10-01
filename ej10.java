488c3f242713ac442dc17b099c56869790369470
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-10-01 14:56:09 +0000

    Subida ej10

---
+++ ej10.java
@@ -0,0 +1,8 @@
+public class ej10 {
+    public static void main(String[] args) {
+        byte numero1 = 120;
+        short numero2 = 32700;
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
+}
