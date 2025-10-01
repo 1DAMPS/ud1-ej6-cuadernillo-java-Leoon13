695ea164870c799960a7031ae92e13e8bce31f3f
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-10-01 14:42:50 +0000

    Subida ej08

---
+++ ej08.java
@@ -0,0 +1,12 @@
+public class ej08 {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        double numero2 = 1.3;
+        char letra = 'D';
+        Boolean primero = true;
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(letra);
+        System.out.println(primero);
+    }
+}
