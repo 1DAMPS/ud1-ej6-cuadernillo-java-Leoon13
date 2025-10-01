aded189dbd4e197613bcb9212ec6eda26638486f
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-10-01 14:47:31 +0000

    Subida ej09

---
+++ ej09.java
@@ -0,0 +1,7 @@
+public class ej09 {
+    public static void main(String[] args) {
+        int numero;
+        System.out.println(numero);
+    }
+}
+//No permite compilar el programa ya que no se ha inicializado y no puede mostrar nada por pantalla.//
