914955111a1b7b0d8291372288522f020387ff01
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-10-07 19:15:18 +0200

    Subida de ej15

---
+++ ej15.java
@@ -0,0 +1,10 @@
+public class ej15 {
+    public static void main(String[] args) {
+        int maximo = 2147483647; // Este es el valor maximo de int en java
+        int resultado = max + 1;
+
+        System.out.println("Resultado: " + resultado);
+
+        // Al sumar 1 al máximo que puede contener un int este se da la vuelta, es decir, va hacia su mínimo que es el número en negativo
+    }
+}
