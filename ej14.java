0fb174ada6ec290d2de1c7b91eac50a54d8f426f
Leoon13 <manuel.leon.ruiz.al@iespoligonosur.org>
2025-10-07 19:11:24 +0200

    Subida ej14

---
+++ ej14.java
@@ -0,0 +1,9 @@
+public class ej14 {
+    public static void main(String[] args) {
+        // Primero declaramos una constante para el número de días de la semana
+        final int DIAS_SEMANA = 7;
+
+        // Finalmente mostramos el mensaje por pantalla
+        System.out.println("La semana tiene " + DIAS_SEMANA + " días.");
+    }
+}
